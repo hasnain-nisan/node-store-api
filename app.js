e739f622e4fbb35b33b899a8408581e562ae8126
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 <hasnain.nisan@outlook.com>
2022-04-05 14:25:41 +0600

    Basic setup done and server is running

--- app.js
+++ app.js
@@ -1,1 +1,42 @@
-console.log('Store API')
+require('dotenv').config();
+
+//async errors
+
+const express = require('express');
+const app = express();
+
+const notFoundMiddleware = require('./middleware/not-found-middleware');
+const errorMiddleware = require('./middleware/error-handler-middleware');
+
+//middleware
+app.use(express.json());
+
+
+//routes
+app.get('/', (req, res) => {
+    res.send(`<h1>Store API</h1>
+        <a href="/api/v1/products">See products</a>
+    `)
+})
+
+//products routes
+
+
+app.use(notFoundMiddleware);
+app.use(errorMiddleware);
+
+
+//port
+const port = process.env.PORT || 5000
+
+const start = async () => {
+    try {
+        //connet DB
+        app.listen(port, console.log(`Server is listeniing on port ${port}`))
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+start();
+
