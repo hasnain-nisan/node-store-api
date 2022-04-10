be2b261204f48481a41241f5df419f2c275bd052
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 <hasnain.nisan@outlook.com>
2022-04-10 13:20:01 +0600

    express-async-errors set error

--- controllers/product.js
+++ controllers/product.js
@@ -2,11 +2,13 @@ const Product = require('../models/Product')
 
 const getAllProductsStatic = async (req, res) => {
     res.status(200).json({msg: 'Product Testing route'})
+
+    //set error
     throw new Error('Test async error pack')
 }
 
 const getAllProducts = async (req, res) => {
-    const product = await Product.find({})
+    const product = await Product.find({company: 'l'})
     res.status(200).json({
         msg: 'Product route',
         product
