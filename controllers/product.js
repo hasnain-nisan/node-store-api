b94f90a95e4a34a69ef75fa02c7b4ef71c5ef5ca
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 <hasnain.nisan@outlook.com>
2022-04-19 14:50:49 +0600

    Sort, select, find, query in mongoDB

--- controllers/product.js
+++ controllers/product.js
@@ -9,7 +9,7 @@ const getAllProductsStatic = async (req, res) => {
 
 const getAllProducts = async (req, res) => {
 
-    const {featured, company, name} = req.query
+    const {featured, company, name, sort, fields} = req.query
     const queryObj = {};
 
     if(featured){
@@ -21,11 +21,28 @@ const getAllProducts = async (req, res) => {
     }
 
     if(name){
-        queryObj.name = name
+        queryObj.name = {$regex: name, $options: 'i'}
     }
 
-    console.log(queryObj);
-    const products = await Product.find(queryObj)
+    const result = Product.find(queryObj)
+    if(sort){
+        const sortLists = sort.split(',').join(" ")
+        console.log(sortLists);
+        result.sort(sortLists)
+    } else {
+        result.sort('createdAt')
+    }
+
+    if(fields){
+        const fieldLists = fields.split(',').join(" ")
+        console.log(fieldLists)
+        result.select(fieldLists)
+    } else {
+        result.select('name price company rating featured createdAt')
+    }
+
+    const products = await result
+
     res.status(200).json({
         msg: 'Product route',
         products,
